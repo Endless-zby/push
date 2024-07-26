9cdac8674841dad29688d0f96b302fc2662d08b5
byzhao <byzhao@rxhui.com>
2024-07-26 15:50:07 +0800

    test

--- src/test/java/cn/byzhao/push/PushApplicationTests.java
+++ src/test/java/cn/byzhao/push/PushApplicationTests.java
@@ -1,13 +1,16 @@
 package cn.byzhao.push;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+@Slf4j
 @SpringBootTest
 class PushApplicationTests {
 
     @Test
-    void contextLoads() {
+    public void contextLoads() {
+        log.info("success...");
     }
 
 }
